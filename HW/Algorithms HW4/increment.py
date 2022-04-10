ab53f54f7bacf6b231c6669c631bfb41317dfc4b
ChromatusX <hong.li12321@gmail.com>
2022-04-09 22:29:25 -0700

    updated alg hw4

--- HW/Algorithms HW4/increment.py
+++ HW/Algorithms HW4/increment.py
@@ -1,47 +1,36 @@
 def increment1(nums):
-    result = [ ]
+    nums.reverse()
     size = len(nums)
+    i = 0
+    nums[0] += 1
+        
+    while i < size:
+        #print(i)
+        #print(size)
+        if i+1 == size and nums[i] == 10:
+            nums.append(0)
+            nums[i] =0
+            nums[i+1] +=1   
 
-    third = nums[size-1]
-    second = nums[size-2]
-    first = nums[size-3]
+        elif nums[i] == 10:
+            nums[i] =0
+            nums[i+1] +=1              
 
-    third  += 1
-    if(third != 10) : 
-        result.append(first)
-        result.append(second)
-        result.append(third)
-    else :
-        third = 0
-        second += 1
-        if(second != 10) :
-            result.append(first)
-            result.append(second)
-            result.append(third)
+        i += 1
 
-        else:
-            second = 0
-            
-            first += 1
-            if first == 10:
-                first = 0
-                result.append(1)
-                result.append(first)
-                result.append(second)
-                result.append(third)
-               
-            else:
-                result.append(first)
-                result.append(second)
-                result.append(third)
+    nums.reverse()
 
-    return result
+    return nums
 
-test1 = [0,9,9]
-test2 = [1,2,9]
-test3 = [1,2,3,4]
+test1 = [0,9,9]   #[1,0,0]
+test2 = [1,2,9]    #[1,3,0]
+test3 = [9,9,9,9]    #[1,0,0,0,0]
+test4 = [9]         #[1,0]
+test5 = [0,0]     #[0,1]
+
 print(increment1(test1))
 print(increment1(test2))
 print(increment1(test3))
-
+print(increment1(test4))
+print(increment1(test5))
  
